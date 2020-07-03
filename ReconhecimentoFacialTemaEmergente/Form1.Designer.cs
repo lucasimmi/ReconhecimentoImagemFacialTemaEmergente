9ff566e294479e64bdd88d73aa1782c6f78e54aa
Lucas Immianovsky <lucas.immi@hotmail.com>
2020-07-03 13:04:29 -0300

    Commit projeto

---
+++ ReconhecimentoFacialTemaEmergente/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ReconhecimentoFacialTemaEmergente
+{
+    partial class FacialSelvi
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.Configurações = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnLigaDesligaCam = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxCamera = new System.Windows.Forms.PictureBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Configurações.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(492, 32);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(500, 410);
+            this.txtLog.TabIndex = 0;
+            this.txtLog.Text = "";
+            // 
+            // Configurações
+            // 
+            this.Configurações.Controls.Add(this.button1);
+            this.Configurações.Controls.Add(this.btnLigaDesligaCam);
+            this.Configurações.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Configurações.Location = new System.Drawing.Point(339, 12);
+            this.Configurações.Name = "Configurações";
+            this.Configurações.Size = new System.Drawing.Size(147, 81);
+            this.Configurações.TabIndex = 1;
+            this.Configurações.TabStop = false;
+            this.Configurações.Text = "Opções";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(6, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Comparar Face";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLigaDesligaCam
+            // 
+            this.btnLigaDesligaCam.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLigaDesligaCam.Location = new System.Drawing.Point(6, 23);
+            this.btnLigaDesligaCam.Name = "btnLigaDesligaCam";
+            this.btnLigaDesligaCam.Size = new System.Drawing.Size(135, 25);
+            this.btnLigaDesligaCam.TabIndex = 4;
+            this.btnLigaDesligaCam.Text = "Identifica foto";
+            this.btnLigaDesligaCam.UseVisualStyleBackColor = true;
+            this.btnLigaDesligaCam.Click += new System.EventHandler(this.btnLigaDesligaCam_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(321, 81);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Temas emergentes: Reconhecimento de imagem";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lucas Immianovsky";
+            // 
+            // pictureBoxCamera
+            // 
+            this.pictureBoxCamera.Location = new System.Drawing.Point(13, 98);
+            this.pictureBoxCamera.Name = "pictureBoxCamera";
+            this.pictureBoxCamera.Size = new System.Drawing.Size(473, 344);
+            this.pictureBoxCamera.TabIndex = 3;
+            this.pictureBoxCamera.TabStop = false;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(917, 4);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 24);
+            this.btnFechar.TabIndex = 5;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FacialSelvi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(1004, 454);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.pictureBoxCamera);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Configurações);
+            this.Controls.Add(this.txtLog);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FacialSelvi";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "FacialSelvi";
+            this.TopMost = true;
+            this.Configurações.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCamera)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.GroupBox Configurações;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxCamera;
+        private System.Windows.Forms.Button btnLigaDesligaCam;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ReconhecimentoFacialTemaEmergente/Form1.cs
@@ -0,0 +1,312 @@
+﻿using Amazon;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReconhecimentoFacialTemaEmergente
+{
+    public partial class FacialSelvi : Form
+    {
+        private CascadeClassifier _rosto;
+        private CascadeClassifier _sorriso;
+        private Capture _imagemCapturada;
+        private float ConfiancaAceitavel = 77f;
+        private int QuantidadeResultados = 5;
+        static bool GatilhoCapturadaFoto = false;
+        static bool GatilhoCapturadaFotoComparacao = false;
+
+        private static BasicAWSCredentials _credentials;
+        private static CredentialProfile _profile;
+
+        public FacialSelvi()
+        {
+            InitializeComponent();
+            InicializaTreinos();
+            InicializaCredenciaisAws();
+        }
+
+        private void InicializaCredenciaisAws()
+        {
+            var options = new CredentialProfileOptions
+            {
+                AccessKey = "CHAVE_AWS",
+                SecretKey = "KEY_AWS"
+            };
+
+            _profile = new Amazon.Runtime.CredentialManagement.CredentialProfile("basic_profile", options);
+            _profile.Region = RegionEndpoint.USEast1;
+            var netSDKFile = new NetSDKCredentialsFile();
+            netSDKFile.RegisterProfile(_profile);
+            _credentials = new BasicAWSCredentials(options.AccessKey, options.SecretKey);
+        }
+
+        public void InicializaTreinos()
+        {
+            _rosto = new CascadeClassifier(Application.StartupPath + "//treinos//haarcascade_frontalface_default.xml");
+            _sorriso = new CascadeClassifier(Application.StartupPath + "//treinos//haarcascade_smile.xml");
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        public string RetornaNomeImagem()
+        {
+            string dataHoraMinuto = DateTime.Now.ToString("yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
+            return "face" + dataHoraMinuto + ".png";
+        }
+
+        public List<string> CompararDuasImagens(Bitmap imagemCapturada)
+        {
+            List<string> mensagensExibir = new List<string>();
+            try
+            {
+                string caminhoImagem = System.IO.Directory.GetCurrentDirectory() + "\\Imagens\\" + RetornaNomeImagem();
+                imagemCapturada.Save(caminhoImagem, ImageFormat.Png);
+                AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient(_credentials, _profile.Region);
+                Amazon.Rekognition.Model.Image imageSource = new Amazon.Rekognition.Model.Image();
+
+                using (FileStream fs = new FileStream(System.IO.Directory.GetCurrentDirectory() + "\\Gabarito\\gabarito.png", FileMode.Open, FileAccess.Read))
+                {
+                    byte[] data = new byte[fs.Length];
+                    fs.Read(data, 0, (int)fs.Length);
+                    imageSource.Bytes = new MemoryStream(data);
+                }
+
+                Amazon.Rekognition.Model.Image imagem = new Amazon.Rekognition.Model.Image();
+                using (FileStream fs = new FileStream(caminhoImagem, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] data = new byte[fs.Length];
+                    data = new byte[fs.Length];
+                    fs.Read(data, 0, (int)fs.Length);
+                    imagem.Bytes = new MemoryStream(data);
+                }
+
+                CompareFacesRequest compareFacesRequest = new CompareFacesRequest()
+                {
+                    SourceImage = imageSource,
+                    TargetImage = imagem,
+                    SimilarityThreshold = ConfiancaAceitavel
+                };
+
+                CompareFacesResponse compareFacesResponse = rekognitionClient.CompareFaces(compareFacesRequest);
+                float posicao = 10;
+                foreach (CompareFacesMatch match in compareFacesResponse.FaceMatches)
+                {
+                    ComparedFace face = match.Face;
+                    BoundingBox position = face.BoundingBox;
+                    var msg = "Detectada face. Sucesso com " + match.Similarity + "% confiança.";
+                    mensagensExibir.Add(msg);
+                    EscreveImagem(caminhoImagem, msg, posicao);
+                    posicao += 30;
+                }
+
+                if (!mensagensExibir.Any())
+                    mensagensExibir.Add("Face não reconhecida");
+
+                return mensagensExibir;
+            }
+            catch (Exception erro)
+            {
+                mensagensExibir.Add("Erro: " + erro.Message);
+                return mensagensExibir;
+            }
+        }
+
+        public void EscreveImagem(string caminhoImagem, string mensagem, float posicao)
+        {
+            Bitmap novaImagem;
+            using (Bitmap bitmap = (Bitmap)System.Drawing.Image.FromFile(caminhoImagem))
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Font arialFont = new Font("Arial", 12))
+                    {
+                        graphics.DrawString(mensagem, arialFont, Brushes.Yellow, new PointF(10f, posicao));
+                    }
+                }
+
+                novaImagem = new Bitmap(bitmap);
+            }
+            novaImagem.Save(caminhoImagem);
+            novaImagem.Dispose();
+        }
+
+        public List<string> ReconhecerImagemCapturada(Bitmap imagemCapturada)
+        {
+            List<string> mensagensExibir = new List<string>();
+            try
+            {
+                string caminhoImagem = System.IO.Directory.GetCurrentDirectory() + "\\Imagens\\" + RetornaNomeImagem();
+                imagemCapturada.Save(caminhoImagem, ImageFormat.Png);
+                Amazon.Rekognition.Model.Image imagem = new Amazon.Rekognition.Model.Image();
+                using (FileStream fs = new FileStream(caminhoImagem, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] data = null;
+                    data = new byte[fs.Length];
+                    fs.Read(data, 0, (int)fs.Length);
+                    imagem.Bytes = new MemoryStream(data);
+                }
+
+                AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient(_credentials, _profile.Region);
+                DetectLabelsRequest detectlabelsRequest = new DetectLabelsRequest()
+                {
+                    Image = imagem,
+                    MaxLabels = QuantidadeResultados,
+                    MinConfidence = ConfiancaAceitavel
+                };
+
+                DetectLabelsResponse detectLabelsResponse = rekognitionClient.DetectLabels(detectlabelsRequest);
+                foreach (Amazon.Rekognition.Model.Label label in detectLabelsResponse.Labels)
+                {
+                    mensagensExibir.Add("Parametro: " + label.Name + "  Grau de confiança: " + label.Confidence);
+                }
+
+                float posicao = 10f;
+                foreach (var mensagem in mensagensExibir)
+                {
+                    EscreveImagem(caminhoImagem, mensagem, posicao);
+                    posicao += 30;
+                }
+            }
+            catch (Exception erro)
+            {
+                mensagensExibir.Add("Erro: " + erro.Message);
+                return mensagensExibir;
+            }
+            return mensagensExibir;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_imagemCapturada == null)
+                    _imagemCapturada = new Capture(1);
+
+                Rectangle gabarito = new Rectangle(0, 0, pictureBoxCamera.Width / 2, pictureBoxCamera.Height / 2);
+                Image<Bgr, byte> img = _imagemCapturada.QueryFrame();
+                using (Image<Bgr, byte> proximoFrame = img)
+                {
+                    if (proximoFrame != null)
+                    {
+                        using (proximoFrame)
+                        {
+                            Image<Gray, byte> frameEstabilizado = proximoFrame.Convert<Gray, byte>();
+                            frameEstabilizado._EqualizeHist();
+                            List<Rectangle> faces = new List<Rectangle>();
+                            List<Rectangle> sorrisosRec = new List<Rectangle>();
+                            using (Image<Gray, byte> frameFinal = frameEstabilizado.Convert<Gray, byte>())
+                            {
+                                if (_rosto != null)
+                                {
+                                    Graphics g = Graphics.FromImage(proximoFrame.Bitmap);
+                                    Pen pen2 = new Pen(Color.Transparent, 10);
+                                    g.DrawRectangle(pen2, gabarito);
+                                    Rectangle[] facesDetected = _rosto.DetectMultiScale(frameFinal, 1.2, 5, new Size(20, 20), Size.Empty);
+                                    faces.AddRange(facesDetected);
+                                    foreach (Rectangle f in facesDetected)
+                                    {
+                                        frameFinal.ROI = f;
+                                        Rectangle[] sorrisos = _sorriso.DetectMultiScale(frameFinal, 4, 20, new Size(20, 20), Size.Empty);
+                                        frameFinal.ROI = Rectangle.Empty;
+                                        foreach (Rectangle sorriso in sorrisos)
+                                        {
+                                            Rectangle sorrisoEc = sorriso;
+                                            sorrisoEc.Offset(f.X, f.Y);
+                                            sorrisosRec.Add(sorrisoEc);
+                                        }
+                                    }
+
+                                    foreach (Rectangle face1 in faces)
+                                        if (faces.Count() > 1)
+                                        {
+                                            face1.Inflate(10, 10);
+                                            proximoFrame.Draw(face1, new Bgr(Color.Red), 2);
+                                            face1.Inflate(20, 20);
+
+                                        }
+                                        else
+                                            proximoFrame.Draw(face1, new Bgr(Color.Green), 2);
+
+                                    if (sorrisosRec.Any())
+                                    {
+                                        var mensagens = ReconhecerImagemCapturada(proximoFrame.ToBitmap());
+                                        foreach (var item in mensagens)
+                                        {
+                                            EscreveLog(item);
+                                        }
+                                    }
+                                    else if (GatilhoCapturadaFoto)
+                                    {
+                                        GatilhoCapturadaFoto = false;
+                                        var mensagens = ReconhecerImagemCapturada(proximoFrame.ToBitmap());
+                                        foreach (var item in mensagens)
+                                        {
+                                            EscreveLog(item);
+                                        }
+                                    }
+                                    else if (GatilhoCapturadaFotoComparacao)
+                                    {
+                                        GatilhoCapturadaFotoComparacao = false;
+                                        var mensagens = CompararDuasImagens(proximoFrame.ToBitmap());
+                                        foreach (var item in mensagens)
+                                        {
+                                            EscreveLog(item);
+                                        }
+                                    }
+
+                                    DesenhaTela(proximoFrame.ToBitmap());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                EscreveLog("Erro: " + ex.Message);
+            }
+        }
+
+        private void DesenhaTela(Bitmap bitmap)
+        {
+            pictureBoxCamera.Image = bitmap;
+        }
+
+        private void EscreveLog(string log)
+        {
+            txtLog.Text += log + " -> " + "\r\n";
+            txtLog.SelectionStart = txtLog.TextLength;
+            txtLog.ScrollToCaret();
+        }
+
+        private void btnLigaDesligaCam_Click(object sender, EventArgs e)
+        {
+            GatilhoCapturadaFoto = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GatilhoCapturadaFotoComparacao = true;
+        }
+    }
+}
